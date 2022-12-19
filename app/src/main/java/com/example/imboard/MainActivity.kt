951bfc085765a95005a63d5f95ec0658a014104b
liadT22 <liadt22@gmail.com>
2022-12-19 23:16:11 +0200

    animation for chaning fragments

--- app/src/main/java/com/example/imboard/MainActivity.kt
+++ app/src/main/java/com/example/imboard/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.imboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.imboard.fragments.AccountFragment
 import com.example.imboard.fragments.NewLobbyFragment
@@ -22,11 +23,18 @@ class MainActivity : AppCompatActivity() {
         val bottom_navigation = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_navigation)
 
         makeCurrentFragment(searchFragment)
+        var currentFragment : Fragment = searchFragment
+        var toFragment : Fragment
 
         bottom_navigation.setOnNavigationItemSelectedListener {
+
+
             when (it.itemId){
                 R.id.ic_search -> makeCurrentFragment(searchFragment)
-                R.id.ic_addRoom -> makeCurrentFragment(newLobbyFragment)
+                R.id.ic_addRoom -> {
+                    toFragment = newLobbyFragment
+                    currentFragment = newLobbyFragment
+                }
                 R.id.ic_account -> makeCurrentFragment(accountFragment)
                 R.id.ic_shop -> makeCurrentFragment(shopFragment)
             }
@@ -35,8 +43,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeCurrentFragment(fragment: Fragment) =
+//        supportFragmentManager.beginTransaction().apply {
+//        replace(R.id.fl_wrapper,fragment)
+//        commit()
+//        }
+
         supportFragmentManager.beginTransaction().apply {
-        replace(R.id.fl_wrapper,fragment)
-        commit()
+            Log.i("fragment",fragment.toString())
+            setCustomAnimations(
+                R.anim.from_left,
+                R.anim.to_right
+            )
+            replace(R.id.fl_wrapper,fragment)
+            commit()
         }
 }
