593b3aeeeeb239691b5bae4cffd44610b451fea5
Cc4Dayz <42410578+Cc4Dayz@users.noreply.github.com>
2022-12-19 20:51:49 +0200

    Bottom navigation bar with fragments for each

--- app/src/main/java/com/example/imboard/MainActivity.kt
+++ app/src/main/java/com/example/imboard/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.imboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.imboard.fragments.AccountFragment
+import com.example.imboard.fragments.NewLobbyFragment
+import com.example.imboard.fragments.SearchFragment
+import com.example.imboard.fragments.ShopFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val accountFragment = AccountFragment()
+        val newLobbyFragment = NewLobbyFragment()
+        val searchFragment = SearchFragment()
+        val shopFragment = ShopFragment()
+        val bottom_navigation = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_navigation)
+
+        makeCurrentFragment(searchFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_search -> makeCurrentFragment(searchFragment)
+                R.id.ic_addRoom -> makeCurrentFragment(newLobbyFragment)
+                R.id.ic_account -> makeCurrentFragment(accountFragment)
+                R.id.ic_shop -> makeCurrentFragment(shopFragment)
+            }
+            true
+        }
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+        replace(R.id.fl_wrapper,fragment)
+        commit()
+        }
 }
