ce618a07640aea9011c287c23d2484f315720243
ofek.a <ofek.atun14@gmail.com>
2023-01-09 19:57:20 +0200

    added firebase added login + register screen

--- app/src/main/java/com/example/imboard/fragments/RegisterOrLoginScreenFragment.kt
+++ app/src/main/java/com/example/imboard/fragments/RegisterOrLoginScreenFragment.kt
@@ -5,21 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
-import com.example.imboard.databinding.LobbyScreenBinding
+import com.example.imboard.databinding.FragmentRegisterOrLoginScreenBinding
 
-class LobbyScreenFragment : Fragment() {
-    private var _binding : LobbyScreenBinding? = null
+class RegisterOrLoginScreenFragment : Fragment() {
+    private var _binding : FragmentRegisterOrLoginScreenBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = LobbyScreenBinding.inflate(inflater, container, false)
-
-        Glide.with(binding.root).load("C:\\Users\\PC\\StudioProjects\\ImBoard\\app\\src\\main\\res\\drawable-v24\\cat_photo.png").circleCrop().into(binding.lobbyGameIcon)
-
+        _binding = FragmentRegisterOrLoginScreenBinding.inflate(inflater, container, false)
 
         return binding.root
     }
