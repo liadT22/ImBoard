08197cfdedbd1f78c9291996cf1bd134ecb0d1bd
liadT22 <liadt22@gmail.com>
2023-02-07 12:47:37 +0200

    changed the way the main 4 fragments works and added animation to all

--- app/src/main/java/com/example/imboard/fragments/NewLobbyFragment.kt
+++ app/src/main/java/com/example/imboard/fragments/NewLobbyFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.imboard.R
 import com.example.imboard.databinding.FragmentNewLobbyBinding
 import com.example.imboard.util.autoCleared
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NewLobbyFragment : Fragment() {
     private var binding : FragmentNewLobbyBinding by autoCleared()
@@ -17,6 +19,16 @@ class NewLobbyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentNewLobbyBinding.inflate(inflater,container,false)
+        requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener()
+        {
+
+            when (it.itemId) {
+                R.id.ic_account -> findNavController().navigate(R.id.action_newLobbyFragment_to_accountFragment)
+                R.id.ic_search -> findNavController().navigate(R.id.action_newLobbyFragment_to_searchFragment)
+                R.id.ic_shop -> findNavController().navigate(R.id.action_newLobbyFragment_to_shopFragment)
+            }
+            true
+        }
         return binding.root
     }
 
