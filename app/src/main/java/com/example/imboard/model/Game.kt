91fe8c39755527b95bfd7e60b3a5737367c219ed
Yanir Na <81169397+YanirNa@users.noreply.github.com>
2023-02-10 16:46:34 +0200

    ?

--- app/src/main/java/com/example/imboard/model/Game.kt
+++ app/src/main/java/com/example/imboard/model/Game.kt
@@ -2,4 +2,4 @@ package com.example.imboard.model
 
 import java.net.URI
 
-data class Game(val Id: String = "",val name: String = "", val MaxPlayerCount: Int = 0, val image: Int = 0)
+data class Game(val Id: String = "",val name: String = "", val MaxPlayerCount: Int = 0, val image: Int = 0)gigi
